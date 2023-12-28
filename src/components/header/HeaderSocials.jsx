44faf087734af4e915aea156ca972941a837fad6
Amr Ezzat <amr.2023001640@buc.edu.eg>
2023-12-29 01:19:14 +0200

    finished the header section

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target='_blank'><FaLinkedin /></a>
+        <a href="https://github.com" target='_blank'><FaGithub/></a>
+        <a href="https://discord.com" target='_blank'><FaDiscord /></a>
+      
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import "./header.css"
-import CTA from './CTA.jsx'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA.jsx";
+import ME from "../../assets/me.png"
+import HeaderSocials from "./HeaderSocials.jsx";
 
 const Header = () => {
   return (
     <header>
-     <div className="container header__container">
-      <h5>Hello, I'm</h5>
-      <h1>Amr Ezzat</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
-      <CTA />
-     </div>
-    </header>
-  )
-}
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Amr Ezzat</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+        <div className="me">
+          <img src= {ME} alt="me" />
+        </div>
 
-export default Header
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
