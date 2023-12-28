87851e343b5bf00e9343054c240097e2380ce019
Amr Ezzat <amr.2023001640@buc.edu.eg>
2023-12-29 00:39:09 +0200

    CHANGES

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,9 +3,14 @@ import "./header.css"
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header>
+     <div className="container header__container">
+      <h5>Hello, I'm</h5>
+      <h1>Amr Ezzat</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      
+     </div>
+    </header>
   )
 }
 
