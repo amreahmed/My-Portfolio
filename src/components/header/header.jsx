459ed9145f2d9a98e4af1e314b6e8744097ed9b8
Amr Ezzat <amr.2023001640@buc.edu.eg>
2023-12-28 19:53:34 +0200

    test2

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./header.css"
+
+const header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default header
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./nav.css"
+const nav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default nav
