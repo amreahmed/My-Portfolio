13a42de9a74b76cf9574b125b583c451f7d09517
Amr Ezzat <amr.2023001640@buc.edu.eg>
2023-12-29 21:07:00 +0200

    adding the review section and the contact section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import { MdOutlineMail } from "react-icons/md";
+import { PiTelegramLogo } from "react-icons/pi";
+import { FaWhatsapp } from "react-icons/fa";
 const contact = () => {
   return (
     
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Tuch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container content__container">
+        <div className="contact__options">
+         <article className="contact__option">
+          <MdOutlineMail/>
+          <h4>Email</h4>
+          <h5>amr43103@gmail.com</h5>
+          <a href="mailto:amr43103@gmail.com" target='_blank'>Send a message</a>
+         </article>
+         <article className="contact__option">
+          <PiTelegramLogo/>
+          <h4>Telegram</h4>
+          <h5>@amreahmed</h5>
+          <a href="https://t.me/amreahmed" target='_blank'>Send a message</a>
+         </article>
+         <article className="contact__option">
+            <FaWhatsapp />
+          <h4>Whatsapp</h4>
+          <h5>+201063806119</h5>
+          <a href="https://api.whatsapp.com/send?phone=201063806119" target='_blank'>Send a message</a>
+         </article>
+        </div>
+        <form action="">
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+        </form>
+      </div>
+    </section>
   
   )
 }
--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import "./testimonials.css"
+import React from "react";
+import "./testimonials.css";
+import IMG from "../../assets/photo_2023-12-29_03-17-56.jpg";
+// import Swiper core and required modules
+import {  Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: IMG,
+    name: "Amr Ezzat",
+    review:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia tempora esse iusto deleniti aperiam non, quibusdam, iste eos amet ipsam nihil voluptatum perferendis quos ratione voluptatem expedita aliquid, repudiandae debitis?"
+  },
+  {
+    avatar: IMG,
+    name: "Amr Ezzat",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia tempora esse iusto deleniti aperiam non, quibusdam, iste eos amet ipsam nihil voluptatum perferendis quos ratione voluptatem expedita aliquid, repudiandae debitis?",
+  },
+  {
+    avatar: IMG,
+    name: "Amr Ezzat",
+    review:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia tempora esse iusto deleniti aperiam non, quibusdam, iste eos amet ipsam nihil voluptatum perferendis quos ratione voluptatem expedita aliquid, repudiandae debitis?",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>
-      Testimonials
-    </div>
-  )
-}
+    <section id="reviews">
+      <h5>Our Clients</h5>
+      <h2>Reviews</h2>
 
-export default Testimonials
+      <Swiper
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}
+       className="container reviews__container">
+        {
+          data.map(({avatar, name, review}) => {
+            return (
+              <SwiperSlide className="reviews">
+          <div className="client__avatar">
+            <img src={avatar} alt="" />
+          </div>
+          <h5 className="client__name">{name}</h5>
+          <small className="client__review">
+           {review}
+          </small>
+        </SwiperSlide>
+            )
+          })
+        }
+        
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
