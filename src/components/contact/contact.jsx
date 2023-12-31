cffeba9445acf0fae203acd14f1b092b87ffda0e
Amr Ezzat <amr.2023001640@buc.edu.eg>
2023-12-31 15:26:53 +0200

    translations

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,9 +5,11 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./contact.css"
 import { useRef } from 'react';
 import emailjs from 'emailjs-com';
-import Swal from 'sweetalert2/dist/sweetalert2';
+import Swal from 'sweetalert2'
+import { useTranslation } from 'react-i18next';
 
 const CONTACT = () => {
+  const { t } = useTranslation();
   const form = useRef();
   const sendEmail = (e) => {
       e.preventDefault();
@@ -15,11 +17,11 @@ const CONTACT = () => {
       emailjs.sendForm('service_vtr18z5', 'template_trd91ej', form.current, 'B-aQGY121V7S7eObe').then(() => {
       
         Swal.fire({
-          title: "Done",
+          title: t("swal"),
           color: "#fff",
           icon: "success",
           background: "#2c2c6c",
-          timer: 2000,
+        
           
         });
       });
@@ -30,35 +32,35 @@ const CONTACT = () => {
   return (
     
     <section id='contact'>
-      <h5>Get In Tuch</h5>
-      <h2>Contact Me</h2>
+      <h5>{t("contact_h5")}</h5>
+      <h2>{t("contact_h2")}</h2>
 
       <div className="container content__container">
         <div className="contact__options">
          <article className="contact__option">
           <MdOutlineMail className='contact__option-icon'/>
-          <h4>Email</h4>
+          <h4>{t("email")}</h4>
           <h5>amr43103@gmail.com</h5>
-          <a href="mailto:amr43103@gmail.com" target='_blank'>Send a message</a>
+          <a href="mailto:amr43103@gmail.com" target='_blank'>{t("message_send")}</a>
          </article>
          <article className="contact__option">
           <PiTelegramLogo className='contact__option-icon'/>
-          <h4>Telegram</h4>
+          <h4>{t("telegram")}</h4>
           <h5>@amreahmed</h5>
-          <a href="https://t.me/amreahmed" target='_blank'>Send a message</a>
+          <a href="https://t.me/amreahmed" target='_blank'>{t("message_send")}</a>
          </article>
          <article className="contact__option">
             <FaWhatsapp className='contact__option-icon'/>
-          <h4>Whatsapp</h4>
+          <h4>{t("whatsapp")}</h4>
           <h5>+201063806119</h5>
-          <a href="https://api.whatsapp.com/send?phone=201063806119" target='_blank'>Send a message</a>
+          <a href="https://api.whatsapp.com/send?phone=201063806119" target='_blank'>{t("message_send")}</a>
          </article>
         </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder='Your Full Name' required/>
-          <input type="email" name="email" placeholder='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type="submit" className='btn btn-primary'>Send Message</button>
+          <input type="text" name="name" placeholder={t("place1")} required/>
+          <input type="email" name="email" placeholder={t("place2")} required/>
+          <textarea name="message" rows="7" placeholder={t("place3")} required></textarea>
+          <button type="submit" className='btn btn-primary'>{t("contact_btn")}</button>
         </form>
       </div>
     </section>
